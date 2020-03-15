5f4c8b0d9d8416d99197080cafdb1cbffde6fffa
Byambaa <Byambaa0325@gmail.com>
2020-03-16 01:14:09 +0800

    Add project lab_6 and initialize ex_1

---
+++ Lab_6/src/Exercise_1_WOLayout.java
@@ -0,0 +1,12 @@
+import java.awt.Frame;
+
+public class Exercise_1_WOLayout  {
+	public static void main(String[] args) {
+		new Exercise_1_WOLayout();
+	}
+	
+	public Exercise_1_WOLayout() {
+		Frame frame = new Frame();
+	}
+
+}
