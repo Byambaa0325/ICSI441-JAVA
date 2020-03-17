b5ca3a64d2c60ee05fada3c77f9df6e8a9f54f1b
Byambaa <Byambaa0325@gmail.com>
2020-03-18 02:00:15 +0800

    Exercise 1

--- Lab_6/src/Exercise_1_WLayout.java
+++ Lab_6/src/Exercise_1_WLayout.java
@@ -1,35 +1,32 @@
+import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.CardLayout;
 import java.awt.Checkbox;
 import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.TextArea;
 import java.awt.TextField;
 import java.util.ArrayList;
 
-public class Exercise_1_WOLayout  {
+public class Exercise_1_WLayout  {
 	private static final int WIDTH = 300;
 	private static final int HEIGHT = 400;
 
 	public static void main(String[] args) {
-		new Exercise_1_WOLayout();
+		new Exercise_1_WLayout();
 	}
 	
-	public Exercise_1_WOLayout() {
+	public Exercise_1_WLayout() {
 		ArrayList<Component> components = getComponents();
 		Frame frame = new Frame();
-		
-		int y = 40;
-		int height = (HEIGHT-40)/components.size();
-		
 		for(Component comp : components) {
-			comp.setBounds(10, y, WIDTH, height);
-			y+=height;
-			frame.add(comp);
+			frame.add(comp,BorderLayout.NORTH);
 		}
-		
+		frame.setLayout(new GridLayout(components.size(),1));
 		frame.setSize(WIDTH, HEIGHT);
-		frame.setLayout(null);
 		frame.setVisible(true);
 		
 	}
