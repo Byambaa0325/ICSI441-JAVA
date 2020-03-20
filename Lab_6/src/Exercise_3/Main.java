c5d19c5c4ba50fc7ca5b47929dbc1280526fe2c9
Byambaa <Byambaa0325@gmail.com>
2020-03-20 18:37:24 +0800

    Refactoring and layout changes

--- Lab_6/src/Exercise_3/Main.java
+++ Lab_6/src/Exercise_3/Main.java
@@ -2,7 +2,6 @@ package Exercise_3;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Frame;
@@ -15,8 +14,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.swing.JPanel;
-
 public class Main {
 
 	private static final int WIDTH = 600;
@@ -26,101 +23,99 @@ public class Main {
 	public static void main(String[] args) {
 		new Main();
 	}
-	
+
 	public Main() {
 		ArrayList<Component> components = getComponents();
 		frame = new Frame();
-		for(Component comp : components) {
-			frame.add(comp,BorderLayout.NORTH);
+		for (Component comp : components) {
+			frame.add(comp, BorderLayout.NORTH);
 		}
-		frame.setLayout(new GridLayout(1,2));
-		frame.setBackground(Color.LIGHT_GRAY);
+		frame.setLayout(new GridLayout(1, 2));
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setVisible(true);
-		
 	}
 
 	private ArrayList<Component> getComponents() {
 		ArrayList<Component> components = new ArrayList<>();
-		
-		//Right
+
+		// Right Panel
 		Panel rightPanel = new Panel();
 		rightPanel.add(new Label("Data"));
-		
+
 		TextArea dataTextField = new TextArea();
 		rightPanel.add(dataTextField);
 		
-		rightPanel.add(new Button("Save"));
-		
-		//Left
+		Panel saveButtonPanel = new Panel();
+		saveButtonPanel.add(new Button("Save"));
+		saveButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		rightPanel.add(saveButtonPanel);
+
+		// Left
 		Panel leftPanel = new Panel();
-		
+
 		leftPanel.add(new Label("Application Form"));
-		JPanel argumentPanel = makeArgumentPanel(new String[] {"First Name","Last Name","Address","E-MailID"});
+		Panel argumentPanel = makeArgumentPanel(new String[] { "First Name", "Last Name", "Address", "E-MailID" });
 		leftPanel.add(argumentPanel);
-		
-		
+
 		Panel buttonsPanel = new Panel();
-		
+
 		Button submitButton = new Button("Submit");
 		Button clearButton = new Button("Clear");
 		Button exitButton = new Button("Exit");
-		
+
 		buttonsPanel.add(submitButton);
-		buttonsPanel.add(clearButton);		
+		buttonsPanel.add(clearButton);
 		buttonsPanel.add(exitButton);
-		
-		submitButton.addActionListener(new ActionListener(){  
-		    public void actionPerformed(ActionEvent e){
-		    	String dataText = "";
-		    	
-		    	for(Component arg : argumentPanel.getComponents()){
-		    		System.out.println(dataText);
-		    		Panel argBox = (Panel) arg;
-		    		Label label = (Label) argBox.getComponent(0);
-		    		TextField tf = (TextField) argBox.getComponent(1);
-		    		dataText+=label.getText()+" "+tf.getText();
-		    		dataText+="\n";
-		    	}
-		    	dataTextField.setText(dataText);
-		    }  
+
+		submitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String dataText = "";
+				for (Component arg : argumentPanel.getComponents()) {
+					Panel argBox = (Panel) arg;
+					Label label = (Label) argBox.getComponent(0);
+					TextField tf = (TextField) argBox.getComponent(1);
+					dataText += label.getText() + " " + tf.getText();
+					dataText += "\n";
+				}
+				dataTextField.setText(dataText);
+			}
 		});
-		clearButton.addActionListener(new ActionListener(){  
-		    public void actionPerformed(ActionEvent e){
-		    	for( Component arg : argumentPanel.getComponents()){
-		    		Panel argBox = (Panel) arg;
-		    		TextField tf = (TextField) argBox.getComponent(1);
-		    		tf.setText("");
-		    	}
-		    	dataTextField.setText("");
-		    }  
+		clearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for (Component arg : argumentPanel.getComponents()) {
+					Panel argBox = (Panel) arg;
+					TextField tf = (TextField) argBox.getComponent(1);
+					tf.setText("");
+				}
+				dataTextField.setText("");
+			}
 		});
-		exitButton.addActionListener(new ActionListener(){  
-		    public void actionPerformed(ActionEvent e){  
-		           frame.dispose();
-		    }  
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
 		});
-		
+
 		buttonsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		leftPanel.add(buttonsPanel);
-		leftPanel.setLayout(new GridLayout(3,1));
-		rightPanel.setLayout(new GridLayout(3,1));
-		
+		leftPanel.setLayout(new GridLayout(3, 1));
+		rightPanel.setLayout(new GridLayout(3, 1));
+
 		components.add(leftPanel);
 		components.add(rightPanel);
-	    
+
 		return components;
 	}
-	
-	private JPanel makeArgumentPanel(String[] labels) {
-		JPanel newPanel = new JPanel();
-		
-		newPanel.setLayout(new GridLayout(labels.length,2));
-		
-		for(String label : labels) {
+
+	private Panel makeArgumentPanel(String[] labels) {
+		Panel newPanel = new Panel();
+
+		newPanel.setLayout(new GridLayout(labels.length, 2));
+
+		for (String label : labels) {
 			Panel box = new Panel();
-			box.setLayout(new GridLayout(1,2));
-			box.add(new Label(label+":"));
+			box.setLayout(new GridLayout(1, 2));
+			box.add(new Label(label + ":"));
 			box.add(new TextField("0.0"));
 			newPanel.add(box);
 		}
